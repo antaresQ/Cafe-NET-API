e1e9f2593e67c2f520688681f3e57935229775aa
antaresQ <antares_elementium@hotmail.com>
2024-09-18 08:55:42 +0800

    fix employee and cafe name clash

--- Data/EmployeeRepository.cs
+++ Data/EmployeeRepository.cs
@@ -33,7 +33,7 @@ namespace Cafe_NET_API.Data
 
         public async Task<IEnumerable<EmployeeDetail>> GetEmployees()
         {
-            string query = $@"SELECT e.id, e.name, e.email_Address, e.phone_number, e.email_Address, e.start_date, c.name
+            string query = $@"SELECT e.id, e.name, e.email_Address, e.phone_number, e.email_Address, e.start_date, c.name as Cafe
                                 FROM CafeEmployee ce
                                 INNER JOIN Cafe c
                                 ON ce.cafe_id = c.id
